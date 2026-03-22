94a15adbcb947cd13a2d3004201e5ef0872523aa
Tijany25 <tijanimudathir@gmail.com>
2026-03-22 23:52:40 +0100

    Update Next.js configuration and dependencies for improved compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
+  // output: "export",
+  // basePath: "/portfolio",
+  // assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
