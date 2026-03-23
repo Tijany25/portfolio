7b96c84b0359d39edcb7f01a163ed78bcaf95660
Tijany25 <tijanimudathir@gmail.com>
2026-03-23 22:30:00 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // basePath: "/portfolio",
-  // assetPrefix: "/portfolio/",
+  output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
